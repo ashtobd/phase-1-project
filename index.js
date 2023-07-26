5b91ac49a6aa84a84a3e040cd43393a0bf4165b4
Brian Ashton <ashtobd@gmail.com>
2023-07-26 10:35:03 -0500

    spellPage function

--- index.js
+++ index.js
@@ -13,14 +13,42 @@ function addSpells(){
       })
     }
 
-    function makeButtons(spells){
-        const buttonsContainer = document.getElementById("buttons")
-        buttonsContainer.innerHTML = ""
-        spells.forEach(element => {
-          const button = document.createElement("button")
-          button.innerText = element.name
-          button.className = "spell button"
-          button.setAttribute("data-spell-name", element.index)
-          button.addEventListener("click", (e) => spellPage(e.target.getAttribute("data-spell-name")))
-                buttonsContainer.appendChild(button)
-        })}
+function makeButtons(spells){
+    const buttonsContainer = document.getElementById("buttons")
+    buttonsContainer.innerHTML = ""
+    spells.forEach(element => {
+        const button = document.createElement("button")
+        button.innerText = element.name
+        button.className = "spell button"
+        button.setAttribute("data-spell-name", element.index)
+        button.addEventListener("click", (e) => spellPage(e.target.getAttribute("data-spell-name")))
+            buttonsContainer.appendChild(button)
+    })}
+
+async function spellPage(target){
+    const spellDetailsContainer = document.getElementById("spell");
+    spellDetailsContainer.innerHTML = '';
+      
+    try {
+        const resp = await fetch(`https://www.dnd5eapi.co/api/spells/${target}/`);
+        const data = await resp.json()
+      
+        const h3 = document.createElement("h3")
+        const p1 = document.createElement("p")
+        const p2 = document.createElement("p")
+        const p3 = document.createElement("p")
+        const p4 = document.createElement("p")
+        h3.innerText = `Spell Name: ${data.name}`
+        p1.innerText = `Range: ${data.range}`
+        p2.innerText = `Casting Time: ${data.casting_time}`
+        p3.innerText = `Components: ${data.components}`
+        p4.innerText = `Description: ${data.desc}`
+        h3.appendChild(p1)
+        p1.appendChild(p2)
+        p2.appendChild(p3)
+        p3.appendChild(p4)
+        spellDetailsContainer.appendChild(h3)
+        h3.id = "spell-name"
+        } catch (error) {
+          console.error('Error fetching spell details:', error)
+      }}
