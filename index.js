8ef62fda2263d18cb6ad29df1911a70a9e60a104
Brian Ashton <ashtobd@gmail.com>
2023-07-26 10:30:45 -0500

    addSpells function

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ async function init() {
   await addSpells()
   filterSearch()
 }
+
+function addSpells(){
+    return fetch('https://www.dnd5eapi.co/api/spells/')
+    .then(resp => resp.json())
+    .then(data => makeButtons(data.results))
+    .catch(error => {
+      console.error('Error fetching spells:', error)
+      })
+    }
